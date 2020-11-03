6eff7be51a2b5fabad8ec4b1a708b974ef17727b
Michael Levitski <=>
2020-11-02 16:24:20 -0800

    fix

--- src/setUpNotifications.js
+++ src/setUpNotifications.js
@@ -2,14 +2,11 @@ const publicVapidKey =
   "BDeetZQiM4kcemNhlUzxQq4MpX-zzVL9pWUbyQNMWjlLASgYFodiKZugM-tRef8NMmHHA_3l-o4Bnx49MQkd8iQ";
 let deferredPrompt = null;
 
-console.log(0)
 window.addEventListener('beforeinstallprompt', e => {
-    console.log('asdasd')
     e.preventDefault()
     deferredPrompt = e
     console.log(deferredPrompt)
 })
-console.log(1)
 
 function urlBase64ToUint8Array(base64String) {
   const padding = "=".repeat((4 - base64String.length % 4) % 4);
@@ -31,9 +28,11 @@ export default async function() {
     try {
         if (!("serviceWorker" in navigator)) throw new Error(errorMessage);
         console.log("Registering service worker...");
-        const register = await navigator.serviceWorker.register("/worker.js", {
+        await navigator.serviceWorker.register("/worker.js", {
           scope: "/"
         });
+        const register = await navigator.serviceWorker.ready
+        console.log(register)
         console.log("Service Worker Registered...");
         console.log("Registering Push...");
     const subscription = await register.pushManager.subscribe({
